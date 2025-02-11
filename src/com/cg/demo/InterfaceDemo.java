cbe7c7c525993639882e6fcb5040065d2eafae1f
Pratikbalodi <PRATIKBALODI.21011349@gehu.ac.in>
2025-02-11 16:59:40 +0530

    first

---
+++ src/com/cg/demo/InterfaceDemo.java
@@ -0,0 +1,11 @@
+package com.cg.demo;
+
+public class InterfaceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("hello....	 world");
+	}
+
+}
